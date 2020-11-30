8f8522dde71695a29ac286d6bbb70be323e2c27f
Francesconliner <francescodivincenzo95@outlook.it>
2020-11-30 11:54:02 +0100

    Header-top

---
+++ main.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: '#root',
+    data: {
+        nav_items: ['Home', 'Pages', 'Courses', 'Features', 'Blog',  'Shop'],
+        },
+    methods: {
+
+    },
+
+    mounted: function(){
+
+    },
+
+})
