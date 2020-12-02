b6e77aee618dbd2a33074cac93888e6424e29f38
Francesconliner <francescodivincenzo95@outlook.it>
2020-12-02 20:39:21 +0100

    Footer e Font

--- main.js
+++ main.js
@@ -2,7 +2,31 @@ var app = new Vue({
     el: '#root',
     data: {
         nav_items: ['Home', 'Pages', 'Courses', 'Features', 'Blog',  'Shop'],
+        footer_items: {
+            address:[
+                '382 NE 191st St # 87394 Miami, FL 33179-3899',
+                '+1 (305) 547-9909 (9am - 5pm EST, Monday - Friday)',
+                'support@maxcoach.com'
+            ],
+            explore:[
+                'Start here',
+                'Success story',
+                'Blog',
+                'Courses',
+                'About us',
+                'Contact us'
+            ],
+            information:[
+                'Membership',
+                'Purchase guide',
+                'Privacy policy',
+                'Terms of services',
+            ]
         },
+        socials: ['fa-facebook-square', 'fa-twitter', 'fa-instagram', 'fa-linkedin'],
+
+    },
+
     methods: {
 
     },
